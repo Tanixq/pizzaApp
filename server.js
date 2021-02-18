b4a08370d9f38b12c6848925c48dfd6b8c85cee4
Tanishq Patel <er.tanixq@gmail.com>
2021-02-19 01:32:39 +0530

    Added login register and cart page

--- server.js
+++ server.js
@@ -4,13 +4,26 @@ const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
 
+app.use(express.static('public'))
+app.use(expressLayout)
+app.set('views', path.join(__dirname, 'resources/views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views', path.join(__dirname, 'resources/views'))
-app.set('view engine', 'ejs')
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
